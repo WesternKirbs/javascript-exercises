b4b349511dd85eeb0784343166813e188fd29a6c
WesternKirbs <benlin96186@gmail.com>
2025-03-19 14:53:41 +0800

    completed assignment 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,19 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...theArgs) {
+    let len = arr.length
+    for(const arg of theArgs){
+        let indexs = [];
+        let count = 0;
+        for(let i = 0;i<len;i++){
+            if(arr[i] === arg){
+                indexs.push(i);
+            }
+        }
+        for(n of indexs){
+            arr.splice(n-count,1);
+            count++;
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line
