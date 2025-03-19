e7d8e243dce31872c871d5f8a1ab604af09d5d19
WesternKirbs <benlin96186@gmail.com>
2025-03-19 16:37:18 +0800

    completed assignment 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,8 @@
-const fibonacci = function() {
-
+const fibonacci = function(nth) {
+    if(nth < 0) return "OOPS";
+    if(nth == 0) return 0;
+    if(nth == 1) return 1;
+    return fibonacci(nth - 1) + fibonacci(nth - 2);
 };
 
 // Do not edit below this line
