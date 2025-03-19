a8af9c2d6bf350ade2ca62ba03931053ac9246fa
WesternKirbs <benlin96186@gmail.com>
2025-03-19 14:15:22 +0800

    completed assignment 03 with all tests passed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,12 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    let index = str.length - 1;
+    res = "";
+    while(index >= 0){
+        res += str[index];
+        index--;
+    }
+    return res;
+
 
 };
 
