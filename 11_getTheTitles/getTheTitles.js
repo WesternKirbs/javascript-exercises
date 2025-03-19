9b6f6c7ae34d7ea821e824ccb663da753ab69f6b
WesternKirbs <benlin96186@gmail.com>
2025-03-19 16:47:40 +0800

    completed assignment 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    let titles = [];
+    for(b of books){
+        titles.push(b.title);
+    }
+    return titles;
 };
 
 // Do not edit below this line
