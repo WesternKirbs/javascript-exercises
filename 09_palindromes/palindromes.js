71a60e9bc6303b1797aef56361abb4c21f46d976
WesternKirbs <benlin96186@gmail.com>
2025-03-19 16:30:59 +0800

    completed assignment 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,22 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    str = str.trim().toLowerCase();
+    first = 0;
+    end = str.length - 1;
+    letters = "abcdefghijklmnopqrstuvwxyz0123456789";
+    while(first < end) {
+        if(!letters.includes(str[first])) {
+            first++;
+            continue;
+        }
+        if(!letters.includes(str[end])) {
+            end--;
+            continue;
+        }
+        if(str[first] != str[end]) return false;
+        first++;
+        end--;
+    }
+    return true;
 };
 
 // Do not edit below this line
