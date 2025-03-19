8bc36d1d8d512bdb890a25ea2bf637d1eeaaef3f
WesternKirbs <benlin96186@gmail.com>
2025-03-19 17:00:13 +0800

    completed assignment 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    max = 0;
+    max_index = 0
+    for(let i = 0;i<people.length;i++) {
+        let death = (!people[i].hasOwnProperty("yearOfDeath")) ? 2025 : people[i].yearOfDeath; 
+        age = death - people[i].yearOfBirth;
+        if(age > max){
+            max = age;
+            max_index = i;
+        }
+    }
+    return people[max_index];
 };
 
 // Do not edit below this line
