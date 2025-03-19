1b0f9babc6a787cb7374b88c35d032ff6fa9cf00
WesternKirbs <benlin96186@gmail.com>
2025-03-19 15:34:55 +0800

    completed assignment 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,8 @@
-const leapYears = function() {
-
+const leapYears = function(y) {
+    if(!(y % 400)) return true;
+    else if(!(y % 100)) return false;
+    else if(!(y % 4)) return true;
+    else return false;
 };
 
 // Do not edit below this line
