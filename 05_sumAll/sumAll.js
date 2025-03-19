4bd6c19e6592c8f7c6a4a56fc681f9c633dbbc49
WesternKirbs <benlin96186@gmail.com>
2025-03-19 15:09:00 +0800

    completed assignment 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,8 @@
-const sumAll = function() {
+const sumAll = function(n1, n2) {
+    if(!Number.isInteger(n1) || !Number.isInteger(n2) || n1<0 || n2<0)
+        return "ERROR";
+
+    return ((n1 + n2) / 2) * (Math.max(n1,n2) - Math.min(n1,n2) + 1);
 
 };
 
